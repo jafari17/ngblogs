4d78fce879a27588071ba4c28b887b1abd2011e6
mostafa <jafari.mostafa@bcmlogic.com>
2024-07-17 23:00:09 +0330

    create

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { SideComponent } from './side/side.component';
+import { ListPostComponent } from './list-post/list-post.component';
+import { AddPostComponent } from './add-post/add-post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    SideComponent,
+    ListPostComponent,
+    AddPostComponent
   ],
   imports: [
     BrowserModule,
